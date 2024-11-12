a49ad61b1e574f659f09cdd4d9c496d4dbf22db4
SushiBar1111 <fachry.altair18@gmail.com>
2024-11-12 20:56:29 +0700

    update admin controller

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -10,7 +10,8 @@ use App\Models\Tool;
 class AdminController extends Controller
 {
     function getAdminDashboard(Request $request){
-        return view('admin.dashboard');
+        $tools = Tool::all();
+        return view('adminDashboard', ['tools' => $tools]);
     }
 
     function addTool(Request $request){
@@ -22,45 +23,45 @@ class AdminController extends Controller
         ]);
 
         if($validation->fails()){
-            return redirect()->route('tool');
+            return redirect()->route('dashboardAdmin')->withErrors('Kureng neh datanya');
         }else{
 
             $toolName = strip_tags($request->input('tool_name'));
             $toolDescription = strip_tags($request->input('description'));
             $toolCategory = strip_tags($request->input('category'));
             //cek apakah udh ada tool dengan nama yg sama
-            $existingTool = Tool::where('name', $toolData->tool_name)->first();
+            $existingTool = Tool::where('name', $toolName)->first();
 
             if($existingTool){
-                return redirect()->route('tool')->with('status', 'udah ada ngab, lupa ya awokwokow');
+                return redirect()->route('dashboardAdmin')->with('status', 'udah ada ngab, lupa ya awokwokow');
             }else{
                 Tool::create([
                     'name' => $toolName,
                     'description' => $toolDescription,
                     'category' => $toolCategory,
-                ])
+                ]);
             }
-            return redirect()->route('tool')->with('status', 'berhasil ditambah ngab');
+            return redirect()->route('dashboardAdmin')->with('status', 'berhasil ditambah ngab');
         }
     }
     function deleteTool(Request $request){
         $validation = Validator::make($request->all(),
         [
-            'tool_id' => 'required|integer';
+            'tool_id' => 'required|integer'
         ]);
 
         if($validation->fails()){
-            return redirect()->route('delete_tool');
+            return redirect()->route('dashboardAdmin'); // kalo validasi fail, ke dashboard admin aja
         }else{
-            $tool = Tool::find('tool_id', $request->tool_id);
+            $tool = Tool::find($request->tool_id)->first();
 
             if(!$tool){
-                return redirect()->route('tool')->with('status', 'lah kok nga ada brok');
+                return redirect()->route('dashboardAdmin')->with('status', 'lah kok nga ada brok');
             }else{
                 $tool->delete();
             }
 
-            return redirect()->route('tool')->with('status', 'berhasil di delete');
+            return redirect()->route('dashboardAdmin')->with('status', 'berhasil di delete');
         }
     }
 }

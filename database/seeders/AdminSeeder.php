bd358311808755d074daabc09a47da819c935aaf
SushiBar1111 <fachry.altair18@gmail.com>
2024-11-13 13:39:45 +0700

    un hardcoded admin password

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -13,7 +13,7 @@ class AdminSeeder extends Seeder
         User::create([
             'username' => 'Administrator',
             'email' => 'admingacor@email.com',
-            'password' => Hash::make('S3m0g@Gu@D@p3tMagangS3b3lum13Des_ember'), // AAMIIN YA ALLAH DAPET MAGANG KALAU BISA LEWAT ENRICHMENT APP LANGSUNG
+            'password' => Hash::make(env('ADMIN_PASSWORD', 'default_password')), // AAMIIN YA ALLAH DAPET MAGANG KALAU BISA LEWAT ENRICHMENT APP LANGSUNG
             'role' => 'admin',
         ]);
     }
